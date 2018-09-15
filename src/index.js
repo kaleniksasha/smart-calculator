99d65dcb09570dedfbe6383ddab9abfa6f1a6235
Kalenik Alexander <kalenik.wr@gmail.com>
2018-09-15 15:20:32 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,26 +1,75 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.expression = [initialValue];
   }
 
   add(number) {
-    // your implementation
+    this.expression.push('+');
+    this.expression.push(number);
+    return this;
   }
-  
+
   subtract(number) {
-    // your implementation
+    this.expression.push('-');
+    this.expression.push(number);
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.expression.push('*');
+    this.expression.push(number);
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.expression.push('/');
+    this.expression.push(number);
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.expression.push('**');
+    this.expression.push(number);
+    return this;
+  }
+
+  valueOf() {
+    let expression = this.expression;
+
+    for (let i = expression.length; i > 0; i--) {
+      if (expression[i - 1] === '**') {
+        expression.splice(i - 2, 3, expression[i - 2] ** expression[i]);
+        i += 1;
+      }
+    }
+
+    for (let i = 0; i < expression.length - 1; i++) {
+      if (expression[i + 1] === '*' || expression[i + 1] === '/') {
+        expression.splice(
+          i,
+          3,
+          expression[i + 1] === '*'
+            ? expression[i] * expression[i + 2]
+            : expression[i] / expression[i + 2]
+        );
+        i -= 1;
+      }
+    }
+
+    for (let i = 0; i < expression.length - 1; i++) {
+      if (expression[i + 1] === '+' || expression[i + 1] === '-') {
+        expression.splice(
+          i,
+          3,
+          expression[i + 1] === '+'
+            ? expression[i] + expression[i + 2]
+            : expression[i] - expression[i + 2]
+        );
+        i -= 1;
+      }
+    }
+
+    return expression[0];
   }
 }
 
